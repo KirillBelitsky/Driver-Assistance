2f135b58a97e99250d41af7d331709e71e7aa003
KirillBelitsky <kirill_belitsky@mail.ru>
2021-01-24 19:59:58 +0300

    add script parser

---
+++ main.py
@@ -0,0 +1,22 @@
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--input', required=True, help='path to input video')
+parser.add_argument('--output', required=True, help='path to output video')
+parser.add_argument('--config', required=True, help='path to yolo config files')
+parser.add_argument('--gpu', required=False, help='GPU Cuda must be used (0 - no, 1 - yes')
+
+args = vars(parser.parse_args())
+
+inputPath = args['input']
+outputPath = args['input']
+gpu = args['gpu']
+gpu = True if gpu is not None and gpu == '1' else False
+
+weightsPath = os.path.sep.join([args["config"], "yolov3.weights"])
+configPath = os.path.sep.join([args["config"], "yolov3.cfg"])
+
+
+
