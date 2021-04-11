f052eac1a903a19f5cce552ecfb135ae78aa108e
KirillBelitsky <kirill_belitsky@mail.ru>
2021-04-11 19:56:06 +0300

    refactor cameraCalibration

--- calibration/cameraCalibration.py
+++ calibration/cameraCalibration.py
@@ -6,34 +6,39 @@ from chessBoard import ChessBoard
 
 # Initialize 20 chessboards
 # note that at instatiation, it finds all chessboard corners and object points
+class CameraCalibration:
 
-chessboards = []
+    def __init__(self):
+        self.chessboards = []
 
-for n in range(20):
-  this_path = '../images/calibrationImages/calibration' + str(n + 1) + '.jpg'
-  chessboard = ChessBoard(i=n, path=this_path, nx=9, ny=6)
-  chessboards.append(chessboard)
+    def calibrate(self):
+        for n in range(20):
+            this_path = '../images/calibrationImages/calibration' + str(n + 1) + '.jpg'
+            chessboard = ChessBoard(i=n, path=this_path, nx=9, ny=6)
+            self.chessboards.append(chessboard)
 
+        points, corners, shape = self.get_data_needed_for_calibration()
 
-# We use these corners and object points (and image dimension)
-# from all the chessboards to calculate the calibration parameters
+        r, matrix, distortion_coef, rv, tv = cv2.calibrateCamera(points, corners, shape, None, None)
 
-points, corners, shape = [], [], chessboards[0].dimensions
+        # Let's store these camera calibration parameters
+        calibration_data = {
+            "camera_matrix": matrix,
+            "distortion_coefficient": distortion_coef
+        }
+        self.save_calibration_data(calibration_data)
 
-for chessboard in chessboards:
-  if chessboard.has_corners:
-    points.append(chessboard.object_points)
-    corners.append(chessboard.corners)
+    # We use these corners and object points (and image dimension)
+    # from all the chessboards to calculate the calibration parameters
+    def get_data_needed_for_calibration(self):
+        points, corners, shape = [], [], self.chessboards[0].dimensions
 
-r, matrix, distortion_coef, rv, tv = cv2.calibrateCamera(points, corners, shape, None, None)
+        for chessboard in self.chessboards:
+            if chessboard.has_corners:
+                points.append(chessboard.object_points)
+                corners.append(chessboard.corners)
 
+        return points, corners, shape
 
-# Let's store these camera calibration parameters
-
-calibration_data = {
-    "camera_matrix": matrix,
-    "distortion_coefficient": distortion_coef
-}
-
-pickle.dump(calibration_data, open("../config/calibration_data.p", "wb"))
-
+    def save_calibration_data(self, calibration_data):
+        pickle.dump(calibration_data, open("../config/calibration_data.p", "wb"))
