7da752ab1b4cad55ccbfa60bec643fef3c63edfa
KirillBelitsky <kirill_belitsky@mail.ru>
2021-04-13 13:02:33 +0300

    fix bug and refactor method

--- util/uiutil.py
+++ util/uiutil.py
@@ -11,3 +11,9 @@ class UiUtil:
         msg.setText(title)
         msg.setInformativeText(message)
         msg.exec()
+
+    @staticmethod
+    def generate_output_video_path(directory, video_path):
+        filename_with_extension = video_path.split('/')[-1]
+        splitted_filename = filename_with_extension.split('.')
+        return directory + '/' + splitted_filename[0] + '_output.avi' #+ splitted_filename[1]
